ded233eb58d46bc6ba850426ed164adb5383f1de
Dami Adesola <damilola.adesola1@gmail.com>
2021-08-22 01:04:26 +0400

    Added Password Strength Meter

--- expensesapp/expensesapp/static/js/register.js
+++ expensesapp/expensesapp/static/js/register.js
@@ -4,6 +4,7 @@ const EmailField=document.querySelector('#EmailField');
 const EmailFeedBackSection=document.querySelector('.email-feedback')
 const togglePassword = document.querySelector('#togglePassword');
 const password = document.querySelector('#PasswordField');
+const strengthBadge = document.querySelector('.password-strength')
 
 togglePassword.addEventListener('click', function (e) {
     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
@@ -57,4 +58,45 @@ EmailField.addEventListener("keyup", (value) => {
             })
     }
     
+});
+
+
+let timeout;
+
+let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
+let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))')
+  
+function StrengthChecker(PasswordParameter){
+    if(strongPassword.test(PasswordParameter) && PasswordParameter.length >= 10) {
+        strengthBadge.style.color = "#fff";
+        strengthBadge.style.background = "#20a820";
+        strengthBadge.textContent = 'Strong';
+    } 
+    else if(mediumPassword.test(PasswordParameter)){
+        strengthBadge.style.color = "#111";
+        strengthBadge.style.background = "#e6da44";
+        strengthBadge.textContent = 'Medium';
+    } 
+    else{
+        strengthBadge.style.color = "#111";
+        strengthBadge.style.background = "#d13636";
+        strengthBadge.textContent = 'Weak';
+    }
+}
+
+  
+
+password.addEventListener("input", () => {
+
+    strengthBadge.style.display= 'block'
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => StrengthChecker(password.value), 250);
+ 
+    if(password.value.length !== 0){
+        strengthBadge.style.display != 'block'
+    } 
+    else{
+        strengthBadge.style.display = 'none'
+    }
 });
