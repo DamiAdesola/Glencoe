7c198112cba6397b6c1566c1054f759ae607d99d
Dami Adesola <damilola.adesola1@gmail.com>
2021-08-21 23:28:11 +0400

    Implemented Warning Messages in Django

--- expensesapp/authentication/views.py
+++ expensesapp/authentication/views.py
@@ -4,10 +4,19 @@ import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from validate_email import validate_email 
+from django.contrib import messages
 # Create your views here.
 
 class RegisterationView(View):
     def get(self,request):
+        return render(request, 'authentication/register.html')
+    
+    def post(self,request):
+        messages.success(request,"Success")
+        messages.info(request,"Success")
+        messages.error(request,"Success")
+        messages.warning(request,"Success")
+        
         return render(request, 'authentication/register.html')
 
 class EmailValidationView(View):
