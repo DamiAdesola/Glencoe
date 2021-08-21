aa0dde7faec3346190524c294fd2e294b3885d66
Dami Adesola <damilola.adesola1@gmail.com>
2021-08-21 22:11:22 +0400

    Implemented Email Validation

--- expensesapp/expensesapp/static/js/register.js
+++ expensesapp/expensesapp/static/js/register.js
@@ -1,15 +1,14 @@
 const UserNameField=document.querySelector('#UserNameField');
-const FeedBackSection=document.querySelector('.invalid-feedback')
+const UsernameFeedBackSection=document.querySelector('.username-feedback')
+const EmailField=document.querySelector('#EmailField');
+const EmailFeedBackSection=document.querySelector('.email-feedback')
 
 UserNameField.addEventListener("keyup", (value) => {
-    console.log('User Typing');
-
     const UserNameValidation = value.target.value;
-    
     UserNameField.classList.remove('is-invalid');
-    FeedBackSection.style.display = "none";
-    
-    if (UserNameValidation.length > 0){9
+    UsernameFeedBackSection.style.display = "none";
+
+    if (UserNameValidation.length > 0){
         fetch("/authentication/validate-username",{
             body:JSON.stringify({username: UserNameValidation}), 
             method:"POST"
@@ -19,8 +18,32 @@ UserNameField.addEventListener("keyup", (value) => {
                 console.log("response",data);
                 if (data.username_error){
                     UserNameField.classList.add('is-invalid');
-                    FeedBackSection.style.display = "block";
-                    FeedBackSection.innerHTML = `<p>${data.username_error}</p> `
+                    UsernameFeedBackSection.style.display = "block";
+                    UsernameFeedBackSection.innerHTML = `<p>${data.username_error}</p> `
+                }
+            })
+    }
+    
+});
+
+
+EmailField.addEventListener("keyup", (value) => {
+    const EmailValidation = value.target.value;
+    EmailField.classList.remove('is-invalid');
+    EmailFeedBackSection.style.display = "none";
+
+    if (EmailValidation.length > 0){
+        fetch("/authentication/validate-email",{
+            body:JSON.stringify({email: EmailValidation}), 
+            method:"POST"
+        })
+            .then(res => res.json())
+            .then((data) => {
+                console.log("response",data);
+                if (data.email_error){
+                    EmailField.classList.add('is-invalid');
+                    EmailFeedBackSection.style.display = "block";
+                    EmailFeedBackSection.innerHTML = `<p>${data.email_error}</p> `
                 }
             })
     }
